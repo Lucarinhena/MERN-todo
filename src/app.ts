87a88810b288918e065a8ef9fc7336efc5a26c63
Luca de Almeida Carinhena <luca.carinhena@falavinha.local>
2024-02-26 16:30:32 -0300

    deploy server

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Express } from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import todoRoutes from './routes'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+app.use(todoRoutes)
+
+const uri:string  = `${process.env.MONGO_CONN}`
+
+mongoose.connect(uri)
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    )
+    .catch((error) => {
+        throw error
+    })
