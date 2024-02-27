9dee69e3a509ec65389d7574737020ed1ef41d87
Luca de Almeida Carinhena <luca.carinhena@falavinha.local>
2024-02-27 09:20:34 -0300

    deploy server

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,14 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import todoRoutes from './routes'
 import dotenv from 'dotenv'
+import bodyParser = require('body-parser')
+
+
 
 dotenv.config()
 const app: Express = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended   : true }));
 
 const PORT: string | number = process.env.PORT || 4000
 
